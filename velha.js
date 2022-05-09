e6494ec0ee525682e88159120d114cae70dccedc
Erikpm16 <102190839+Erikpm16@users.noreply.github.com>
2022-05-08 23:17:59 -0300

    Add files via upload

---
+++ velha.js
@@ -0,0 +1,198 @@
+var jogador, vencedor = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+var vencedorSelecionado = document.getElementById('vencedor-selecionado');
+
+mudarJogador('X');
+
+function escolherQuadrado(id) {
+    if (vencedor !== null) {
+        return;
+    }
+
+    var quadrado = document.getElementById(id);
+    if (quadrado.innerHTML !== '-') {
+        return;
+    }
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+
+    if (jogador === 'X') {
+        jogador = 'O';
+    } else {
+        jogador = 'X';
+    }
+
+    mudarJogador(jogador);
+    checaVencedor();
+}
+
+function mudarJogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+}
+
+function checaVencedor() {
+    var quadrado1 = document.getElementById(1);
+    var quadrado2 = document.getElementById(2);
+    var quadrado3 = document.getElementById(3);
+    var quadrado4 = document.getElementById(4);
+    var quadrado5 = document.getElementById(5);
+    var quadrado6 = document.getElementById(6);
+    var quadrado7 = document.getElementById(7);
+    var quadrado8 = document.getElementById(8);
+    var quadrado9 = document.getElementById(9);
+
+    if (checaSequencia(quadrado1, quadrado2, quadrado3)) {
+        mudaCorQuadrado(quadrado1, quadrado2, quadrado3);
+        mudarVencedor(quadrado1);
+        return;
+    }
+
+    if (checaSequencia(quadrado4, quadrado5, quadrado6)) {
+        mudaCorQuadrado(quadrado4, quadrado5, quadrado6);
+        mudarVencedor(quadrado4);
+        return;
+    }
+
+    if (checaSequencia(quadrado7, quadrado8, quadrado9)) {
+        mudaCorQuadrado(quadrado7, quadrado8, quadrado9);
+        mudarVencedor(quadrado7);
+        return;
+    }
+
+    if (checaSequencia(quadrado1, quadrado4, quadrado7)) {
+        mudaCorQuadrado(quadrado1, quadrado4, quadrado7);
+        mudarVencedor(quadrado1);
+        return;
+    }
+
+    if (checaSequencia(quadrado2, quadrado5, quadrado8)) {
+        mudaCorQuadrado(quadrado2, quadrado5, quadrado8);
+        mudarVencedor(quadrado2);
+        return;
+    }
+
+    if (checaSequencia(quadrado3, quadrado6, quadrado9)) {
+        mudaCorQuadrado(quadrado3, quadrado6, quadrado9);
+        mudarVencedor(quadrado3);
+        return;
+    }
+
+    if (checaSequencia(quadrado1, quadrado5, quadrado9)) {
+        mudaCorQuadrado(quadrado1, quadrado5, quadrado9);
+        mudarVencedor(quadrado1);
+        return;
+    }
+
+    if (checaSequencia(quadrado3, quadrado5, quadrado7)) {
+        mudaCorQuadrado(quadrado3, quadrado5, quadrado7);
+        mudarVencedor(quadrado3);
+    }
+}
+
+function mudarVencedor(quadrado) {
+    vencedor = quadrado.innerHTML;
+    vencedorSelecionado.innerHTML = vencedor;
+}
+
+function mudaCorQuadrado(quadrado1, quadrado2, quadrado3) {
+    quadrado1.style.background = '#0f0';
+    quadrado2.style.background = '#0f0';
+    quadrado3.style.background = '#0f0';
+}
+
+function checaSequencia(quadrado1, quadrado2, quadrado3) {
+    var eigual = false;
+
+    if (quadrado1.innerHTML !== '-' && quadrado1.innerHTML === quadrado2.innerHTML && quadrado2.innerHTML === quadrado3.innerHTML) {
+        eigual = true;
+    }
+
+    return eigual;
+}
+
+function reiniciar() {
+    vencedor = null;
+    vencedorSelecionado.innerHTML = '';
+
+    for (var i = 1; i <= 9; i++) {
+        var quadrado = document.getElementById(i);
+        quadrado.style.background = '#eee';
+        quadrado.style.color = '#eee';
+        quadrado.innerHTML = '-';
+    }
+
+    mudarJogador('X');
+}
+
+
+
+
+var jogadores = [
+    {
+        id: 1,
+        avatar: avatarList[0].src,
+        nome: "X",
+        vitorias: 0,
+        empates: 0,
+        derrotas: 0,
+        pontos: 0
+    },
+    {
+        id: 2,
+        avatar: avatarList[1].src,
+        nome: "O",
+        vitorias: 0,
+        empates: 0,
+        derrotas: 0,
+        pontos: 0
+    }];
+
+function calculaPontos(jogador) {
+    var pontos = jogador.vitorias * 3 + jogador.empates;
+    return pontos;
+}
+
+jogadores[0].pontos = calculaPontos(jogadores[0]);
+jogadores[1].pontos = calculaPontos(jogadores[1]);
+jogadores[2].pontos = calculaPontos(jogadores[2]);
+
+function exibeJogadoresNaTela(jogadores) {
+    var elemento = "";
+    for (var i = 0; i < jogadores.length; i++) {
+        elemento += "<tr><td><img src=" + jogadores[i].avatar + "></td>";
+        elemento += "<td>" + jogadores[i].nome + "</td>";
+        elemento += "<td>" + jogadores[i].vitorias + "</td>";
+        elemento += "<td>" + jogadores[i].empates + "</td>";
+        elemento += "<td>" + jogadores[i].derrotas + "</td>";
+        elemento += "<td>" + jogadores[i].pontos + "</td>";
+        
+    }
+
+    var tabelaJogadores = document.getElementById("placar");
+    tabelaJogadores.innerHTML = elemento;
+}
+
+exibeJogadoresNaTela(jogadores);
+
+function reset() {
+    for (var i = 0; i < jogadores.length; i++) {
+        jogadores[i].vitorias = 0;
+        jogadores[i].empates = 0;
+        jogadores[i].derrotas = 0;
+        jogadores[i].pontos = 0;
+    }
+    exibeJogadoresNaTela(jogadores);
+}
+
+
+function addPonto() {
+    if (checaVencedor === X) {
+        var jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+    };
+
+}
